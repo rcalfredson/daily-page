f167e4c8c68061ad04f547b77bacc4ea828cf101
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2018-11-01 10:14:38 -0400

    Add means to save daily document to DB.

--- server/jwt-helper.js
+++ server/jwt-helper.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-function expiringKey(expiryMins = 2) {
+function expiringKey(expiryMins = 1440) {
   return jwt.sign({ exp: Math.floor(Date.now() / 1000) + (expiryMins * 60) }, process.env.APP_AUTH);
 }
 
--- server/mongo.js
+++ server/mongo.js
@@ -1,16 +1,17 @@
 const { MongoClient } = require('mongodb');
+const moment = require('moment-timezone');
 
 const user = 'daily-page-admin';
 const pw = process.env.MONGO_DB_PW;
 const url = `mongodb+srv://${user}:${pw}@dailypage-olyqk.gcp.mongodb.net/test?retryWrites=true`;
 const dbName = 'daily-page';
-const collectionName = 'session-data';
+const collectionNames = { session: 'session-data', pages: 'pages' };
+const collections = { session: null, pages: null };
 
-let collection;
 let connection;
 let db;
 
-async function init() {
+async function initDB() {
   const connectTimeoutMS = process.env.MONGODB_CONNECT_TIMEOUT || 15000;
   const socketTimeoutMS = process.env.MONGODB_SOCKET_TIMEOUT || 30000;
 
@@ -21,30 +22,46 @@ async function init() {
     socketTimeoutMS,
   });
   db = await connection.db(dbName);
-  collection = db.collection(collectionName);
+}
+
+async function initCollection(name) {
+  await initDB();
+  collections[name] = await db.collection(collectionNames[name]);
+}
+
+async function initPagesCollection() {
+  await initCollection('pages');
+}
+
+async function initSessionCollection() {
+  await initCollection('session');
 }
 
 async function peerIDs() {
-  await init();
-
-  return (await collection.findOne({ _id: peerIDs })).peerIDs;
+  await initSessionCollection();
+  return (await collections.session.findOne({ _id: peerIDs })).peerIDs;
 }
 
 async function addPeer(id) {
-  await init();
-
-  return collection.updateOne({ _id: peerIDs }, { $push: { peerIDs: id } });
+  await initSessionCollection();
+  return collections.session.updateOne({ _id: peerIDs }, { $push: { peerIDs: id } });
 }
 
 async function removePeer(id) {
-  await init();
+  await initSessionCollection();
+  return collections.session.updateOne({ _id: peerIDs }, { $pull: { peerIDs: id } });
+}
 
-  return collection.updateOne({ _id: peerIDs }, { $pull: { peerIDs: id } });
+async function updatePage(content) {
+  await initPagesCollection();
+  const currentDate = moment(Date.now()).tz('Europe/London').format('YYYY-MM-DD');
+  return collections.pages
+    .updateOne({ _id: currentDate }, { $set: { content } }, { upsert: true });
 }
 
 module.exports = {
-  init,
   peerIDs,
   addPeer,
   removePeer,
+  updatePage,
 };
