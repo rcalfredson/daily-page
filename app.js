5d7392a17c47198a41c9ada62cd685f1b6654b95
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2018-11-03 11:47:52 -0400

    Add placeholder for new doc-archive view and remove more Conclave references from code.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/', async (req, res) => {
   const peerIDs = await cache.get('peerIDs', mongo.peerIDs);
   if (Object.keys(req.query).length !== 0 || peerIDs.length === 0) {
     res.render('index', {
-      title: 'Conclave',
+      title: 'Daily Page',
       backendURL,
       sessionID: jwtHelper.expiringKey(),
     });
@@ -29,14 +29,9 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/:date([0-9]{4}-[0-9]{2}-[0-9]{2})', async (req, res) => {
-  let content;
-
-  try {
-    content = (await mongo.getPage(req.params.date)).replace(/\n/g, '<br>');
-  } catch (error) {
-    res.status(404).send('We cannot find that page.');
-  }
-  res.send(content);
+  res.render('archivedPage', {
+    title: `Daily Page for ${req.params.date}`
+  })
 });
 
 const srv = app.listen(port, () => {
