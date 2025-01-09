e8ab6e64123033a4c35fdca92506661e834360f3
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2025-01-08 23:50:01 -0500

    Add start of a toolbar to the editor, with one button - for adding im… (#68)
    * Add start of a toolbar to the editor, with one button - for adding images easily to the canvas.
    
    * Add trailing newlines where needed, and delete commented-out code.

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const toolbar = document.querySelector('.toolbar.sticky');
+  const editor = document.querySelector('.EasyMDEContainer');
+  const buttonsWithTooltips = document.querySelectorAll('[data-tooltip]');
+
+  const toolbarHeight = toolbar.offsetHeight;
+
+  window.addEventListener('scroll', () => {
+    const editorTop = editor.getBoundingClientRect().top;
+
+    if (editorTop < toolbarHeight) {
+      toolbar.classList.add('scrolled');
+    } else {
+      toolbar.classList.remove('scrolled');
+    }
+  });
+
   const descriptionHeader = document.querySelector('.room-description-header');
   const description = document.querySelector('.room-description');
 
