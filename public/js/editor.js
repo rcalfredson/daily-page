6a17bdfa242005e3d5b075714f0ab379411527a2
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2025-01-08 23:49:30 -0500

    Add trailing newlines where needed, and delete commented-out code.

--- public/js/editor.js
+++ public/js/editor.js
@@ -24,26 +24,4 @@ document.addEventListener('DOMContentLoaded', () => {
       description.classList.toggle('visible', !isVisible);
     });
   }
-
-  // buttonsWithTooltips.forEach((button) => {
-  //   button.addEventListener('mouseenter', () => {
-  //     const tooltipText = button.getAttribute('data-tooltip');
-  //     const tooltip = document.createElement('div');
-
-  //     // Create Tooltip Element
-  //     tooltip.className = 'dynamic-tooltip';
-  //     tooltip.innerText = tooltipText;
-  //     document.body.appendChild(tooltip);
-
-  //     // Position Tooltip Below Button
-  //     const buttonRect = button.getBoundingClientRect();
-  //     tooltip.style.position = 'absolute';
-  //     tooltip.style.left = `${buttonRect.left + buttonRect.width / 2 - tooltip.offsetWidth / 2}px`;
-  //     tooltip.style.top = `${buttonRect.bottom + 10}px`; // Always below button
-
-  //     button.addEventListener('mouseleave', () => {
-  //       tooltip.remove();
-  //     });
-  //   });
-  // });
 });
