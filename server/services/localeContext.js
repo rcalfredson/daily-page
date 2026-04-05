5c7820109e0f1d88cefcae6ddea23d946a792ebd
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2026-04-05 16:30:10 -0400

    Add full Russian UI i18n support

--- server/services/localeContext.js
+++ server/services/localeContext.js
@@ -1,6 +1,6 @@
 // server/services/localeContext.js
 
-export const SUPPORTED_UI_LANGS = ['en', 'es'];
+export const SUPPORTED_UI_LANGS = ['en', 'es', 'ru'];
 export const DEFAULT_UI_LANG = 'en';
 
 export function isSupportedUiLang(l) {
