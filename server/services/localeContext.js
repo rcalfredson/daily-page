bfcefa58526b8134b4961580760307c52294a01c
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2026-04-30 22:59:12 -0400

    Add Italian UI locale support

--- server/services/localeContext.js
+++ server/services/localeContext.js
@@ -1,6 +1,6 @@
 // server/services/localeContext.js
 
-export const SUPPORTED_UI_LANGS = ['en', 'es', 'fr', 'ru', 'id', 'de'];
+export const SUPPORTED_UI_LANGS = ['en', 'es', 'fr', 'ru', 'id', 'de', 'it'];
 export const DEFAULT_UI_LANG = 'en';
 
 export function isSupportedUiLang(l) {
