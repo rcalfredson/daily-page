92194db5295be7c3730d94ad325719ed47b1b9bf
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2026-04-30 22:34:39 -0400

    Add German UI locale support

--- server/services/localeContext.js
+++ server/services/localeContext.js
@@ -1,6 +1,6 @@
 // server/services/localeContext.js
 
-export const SUPPORTED_UI_LANGS = ['en', 'es', 'fr', 'ru', 'id'];
+export const SUPPORTED_UI_LANGS = ['en', 'es', 'fr', 'ru', 'id', 'de'];
 export const DEFAULT_UI_LANG = 'en';
 
 export function isSupportedUiLang(l) {
